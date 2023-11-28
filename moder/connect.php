1bc735b609dcc101dca24dbc0ad393736cfdfc43
vinhmq-ndm <vinhmqph34135@fpt.edu.vn>
2023-11-28 10:59:49 +0700

    update pdo

--- moder/connect.php
+++ moder/connect.php
@@ -1,7 +1,7 @@
 <?php
 function connect()
 {
-    $conn=new PDO("mysql:hostname=localhost;post=3306;dbname=wb_du_an_1",'root',"");
+    $conn=new PDO("mysql:hostname=localhost;post=3306;dbname=du_an_1",'root',"");
     $conn->exec("set names utf8");
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
     return $conn;
